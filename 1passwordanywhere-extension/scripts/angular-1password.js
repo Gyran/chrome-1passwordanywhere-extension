a06306acbf07bba108ac56ad836869861e5013cf
Gustav Ahlberg <Gustav.Ahlberg@gmail.com>
2014-05-05 17:02:39 +0200

    Made OnePassword into an only angular module

--- 1passwordanywhere-extension/scripts/angular-1password.js
+++ 1passwordanywhere-extension/scripts/angular-1password.js
@@ -2,7 +2,7 @@
     "use strict";
 
     angular.module("ng1password", []);
-    angular.module("ng1password").service("$1password", function ($q, $window) {
+    angular.module("ng1password").service("$1password", function ($q) {
         var onepassword;
 
         this.init = function (dp) {
@@ -40,6 +40,89 @@
             onepassword.decrypt(item);
             return item;
         };
+
+	function OnePassword (dp) {
+	    this.dataProvider = dp;
+	    this.keychain = new Keychain();
+	}
+
+	OnePassword.prototype.load = function () {
+	    var deferred = $q.defer();
+
+	    $q.all([this.getContents(), this.getEncryptionKeys()]).then(function () {
+		deferred.resolve();
+	    }, function () {
+		deferred.reject();
+	    });
+
+	    return deferred.promise;
+	};
+	OnePassword.prototype.getContents = function () {
+	    var that = this;
+	    var deferred = $q.defer();
+
+	    this.dataProvider.getContents().then(function (contents) {
+		that.keychain.setContents(contents);
+		deferred.resolve();
+	    }, function () {
+		deferred.reject();
+	    });
+
+	    return deferred.promise;
+	};
+	OnePassword.prototype.getEncryptionKeys = function () {
+	    var that = this;
+	    var deferred = $q.defer();
+
+	    that.dataProvider.getEncryptionKeys().then(function (keys) {
+		that.keychain.setEncryptionKeys(keys);
+		deferred.resolve();
+	    }, function () {
+		deferred.reject();
+	    });
+
+	    return deferred.promise;
+	};
+
+	OnePassword.prototype.verifyPassword = function (password) {
+	    return this.keychain.verifyPassword(password);
+	};
+
+	OnePassword.prototype.getKeychainItem = function (entryid) {
+	    var that = this;
+	    var deferred = $q.defer();
+
+	    that.dataProvider.getKeychainItem(entryid).then(function (entry) {
+		deferred.resolve(new KeychainItem(entry, that.keychain));
+	    });
+
+	    return deferred.promise;
+	};
+
+	OnePassword.prototype.webformsWithDomain = function (domain) {
+	    var ret = [];
+
+	    ret = this.keychain.contents[TYPE_WEBFORMS].filter(function (webform) {
+		try {
+		    var url = new URL(webform.domain);
+		    var webformDomain = tld.getDomain(url.hostname);
+
+		    if (domain === webformDomain) {
+			return true;
+		    }
+		} catch (err) {
+		    console.log("ERROR! when trying to populate webforms", err, webform);
+		}
+		return false;
+	    });
+
+	    return ret;
+	};
+
+	OnePassword.prototype.decrypt = function (keychainItem) {
+	    keychainItem.decrypt(this.keychain);
+	};
+
 
     });
 
